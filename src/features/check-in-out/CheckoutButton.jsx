c78fbdeea380ae62959a1d8ed302d675fdd2eaec
nosaemma21 <emmanuelnosakhare052@gmail.com>
2024-10-12 08:39:48 +0100

    Few stuff changed

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,11 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "../../features/check-in-out/useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isLoading } = useCheckout(bookingId);
+
   return (
-    <Button variation="primary" size="small">
+    <Button variation="danger" size="small" onClick={() => checkout(bookingId)}>
       Check out
     </Button>
   );
